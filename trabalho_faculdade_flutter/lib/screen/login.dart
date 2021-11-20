99782e2a60d9dcdf6c8ef6a78d0d07ed71d219cd
Pedro Leal <pedrohenrileal@hotmail.com>
2021-11-20 15:03:21 -0300

    njkn

--- trabalho_faculdade_flutter/lib/screen/login.dart
+++ trabalho_faculdade_flutter/lib/screen/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -8,6 +9,32 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreeneState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login"),
+      ),
+      body: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: TextField(),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: TextField(),
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+            RaisedButton(
+                color: Theme.of(context).accentColor,
+                child: Text('Signin'),
+                onPressed: () {}),
+            RaisedButton(
+                color: Theme.of(context).accentColor,
+                child: Text('Signup'),
+                onPressed: () {})
+          ]),
+        ],
+      ),
+    );
   }
 }
--- trabalho_faculdade_flutter/lib/src/app.dart
+++ trabalho_faculdade_flutter/lib/src/app.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:trabalho_faculdade_flutter/screen/login.dart';
 
 class App extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Login App',
