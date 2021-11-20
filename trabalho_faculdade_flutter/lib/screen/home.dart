6c763ae219a97f581ba10098eaa06da1449d4221
Izabella Matos <izabella.matos@dtidigital.com.br>
2021-11-20 14:43:15 -0300

    pega

--- trabalho_faculdade_flutter/lib/screen/home.dart
+++ trabalho_faculdade_flutter/lib/screen/home.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
-class HomeScreen extends StatelessWidget {
-  const name({Key? key}) : super(key: key);
 
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container();
