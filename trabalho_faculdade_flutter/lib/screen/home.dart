3705c104ff58527e90d74060194aaa6b210a60e9
Pedro Leal <pedrohenrileal@hotmail.com>
2021-11-21 23:12:32 -0300

    rgrgdr

--- trabalho_faculdade_flutter/lib/screen/home.dart
+++ trabalho_faculdade_flutter/lib/screen/home.dart
@@ -3,29 +3,34 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+  late FirebaseFirestore db = FirebaseFirestore.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-          TextButton(
-              onPressed: () {
-                Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (context) => HomeScreen()));
-              },
-              child: Text('Voltar')),
-          StreamBuilder(
-              stream:
-                  FirebaseFirestore.instance.collection('cliente').snapshots(),
-              builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                return ListView(
-                    // children: snapshot.data?.docs.map((cliente) {
-                    //   return Center();
-                    // }).toList(),
-                    );
-              })
+          FutureBuilder(
+            future: db.collection('cliente').doc('VJmxTX7i22mJG4m9xoVn').get(),
+            builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+              if (snapshot.hasData) {
+                Map<String, dynamic> data =
+                    snapshot.data!.data() as Map<String, dynamic>;
+                return Text("Full Name: ${data['nome']} ${data['cpf']}");
+              } else {
+                return Text('nao tem dados');
+              }
+            },
+          )
         ],
       ),
     );
+  }
+
+  lerBanco() async {
+    final snapShot =
+        await db.collection('cliente').doc('VJmxTX7i22mJG4m9xoVn').get();
+
+    return snapShot;
   }
 }
